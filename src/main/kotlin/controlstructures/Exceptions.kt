c2e9333f10a838628d4389220247d3d6cf90305e
Gregory Risch <g9@exostaz.eu>
2022-12-29 14:06:05 +0100

    Arranged Code

---
+++ src/main/kotlin/controlstructures/Exceptions.kt
@@ -0,0 +1,4 @@
+package controlstructures
+
+class Exceptions {
+}
--- src/main/kotlin/controlstructures/loops.kt
+++ src/main/kotlin/controlstructures/loops.kt
@@ -6,22 +6,20 @@ fun main() {
         print("while ")
         i++
     }
-
-    println("\nafter the while i = $i")
+    println()
 
     do {
         i--
         print("do ")
     } while (i >= 1)
-
     println()
 
     val list = listOf<String>("a", "b", "c")
     for (s in list) {
         print(s)
     }
-
     println()
+
     val map = mapOf<Int, String>(
         1 to "One",
         2 to "Two",
@@ -35,21 +33,20 @@ fun main() {
     for ((index, element) in list.withIndex()) {
         println("$index = $element")
     }
-    println()
 
     val range = 1..9
     for (j in range) print(j)
-
     println()
+
     val untilWeReach = 0 until 10
     for (k in untilWeReach) print(k)
-
     println()
+
     for (l in 9 downTo 0 step 2) print(l)
-
     println()
+
     for (ch in 'a'..'z') print(ch)
-
     println()
+
     for (ch in 'a'..'d') print(ch + 1)
 }
